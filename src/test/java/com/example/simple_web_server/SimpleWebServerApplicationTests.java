2c92be46a03a2cb4b0eda24f83ed54b2097e0ace
ankit-majumder <ankit.majumder.tx@gmail.com>
2025-12-31 11:39:32 -0600

    Initial Commit

---
+++ src/test/java/com/example/simple_web_server/SimpleWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_web_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
